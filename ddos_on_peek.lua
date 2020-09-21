ca5ef53dfeb7eb0033f0a897601a2dc52384b4c5
felix <rafaelbadas123@gmail.com>
2020-09-21 02:23:12 -0300

    Updated to latest local version

--- ddos_on_peek.lua
+++ ddos_on_peek.lua
@@ -1,6 +1,8 @@
 local sv_maxunlag_ref = ui.reference("MISC", "Settings", "sv_maxunlag") -- reference for sv_maxunlag
 local sv_maxusrcmdprocessticks_ref = ui.reference("MISC", "Settings", "sv_maxusrcmdprocessticks") -- reference for sv_maxusrcmdprocessticks
 local fakelag_limit_ref = ui.reference("AA", "Fake lag", "Limit") -- reference for fakelag limit
+local fakelag_enabled_ref = ui.reference("AA", "Fake lag", "Enabled") -- reference for fakelag enable
+local leg_movement_ref = ui.reference("AA", "Other", "Leg movement") -- reference for leg movement
 local fakeduck_ref = ui.reference("RAGE", "Other", "Duck peek assist") -- reference for fakeduck
 
 -- caching convars for easier access
@@ -9,7 +11,7 @@ local convars = {
     ["sv_maxunlag"] = cvar["sv_maxunlag"]
 }
 
--- making chockedcommands global for paint event
+-- making chokedcommands global for paint event
 local choked_commands = {amount = 0}
 
 -- setting both hidden elements visible
@@ -55,28 +57,42 @@ client.set_event_callback(
             ui.set(fakelag_limit_ref, ui.get(sv_maxusrcmdprocessticks_ref) - 2)
         end
 
-        choked_commands.amount = e.chokedcommands
+        if (e.chokedcommands > 16) then
+            choked_commands.amount = e.chokedcommands
+            ui.set(leg_movement_ref, "Always slide")
+        else
+            choked_commands.amount = 0
+            ui.set(leg_movement_ref, "Never slide")
+        end
     end
 )
 
 client.set_event_callback(
     "paint",
     function()
-        local percentage = choked_commands.amount * 100 / ui.get(sv_maxusrcmdprocessticks_ref)
+        if (ui.get(fakelag_enabled_ref) and entity.is_alive(entity.get_local_player())) then
+            local percentage = choked_commands.amount * 100 / ui.get(sv_maxusrcmdprocessticks_ref)
 
-        local y = renderer.indicator(220, 220, 220, 255, string.format("lag %d%%", percentage))
-        local w, h = renderer.measure_text("b", string.format("lag %d%%", percentage))
+            local color = {
+                -- thanks to rave
+                255 - (percentage / 1.62 * 2.29824561404),
+                percentage / 1.62 * 3.42105263158,
+                percentage / 1.62 * 0.22807017543
+            }
 
-        -- what the fuck even are these numbers
-        renderer.circle_outline(w + 70, y + 19, 0, 0, 0, 150, 10, 0, 1, 5)
-        renderer.circle_outline(w + 70, y + 19, 220, 220, 220, 255, 9, 0, percentage / 100, 4)
+            local vert = renderer.indicator(color[1], color[2], color[3], 255, "PEEK")
+
+            -- what the fuck even are these numbers
+            renderer.circle_outline(90, vert + 19, 0, 0, 0, 150, 10, 0, 1, 5)
+            renderer.circle_outline(90, vert + 19, color[1], color[2], color[3], 255, 9, 0, percentage / 100, 4)
+        end
     end
 )
 
 client.set_event_callback(
     "shutdown",
     function()
-        -- setting both hidden elements invisible on shutdown
+        -- setting both hidden elements invisible upon shutdown
         ui.set_visible(sv_maxunlag_ref, false)
         ui.set_visible(sv_maxusrcmdprocessticks_ref, false)
     end
